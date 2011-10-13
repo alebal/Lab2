a2fcd121ca38ef961175bc84cd9d71ffdbc573b1
holy_andrei <holy_andrei@yahoo.com>
2011-10-13 16:55:30 +0300

    adaugat asd.pl

---
+++ asd.pl
@@ -0,0 +1,11 @@
+# linia urmatoare spune interpretorului sa afiseze
+# mesaje de atentionare
+use warnings;
+
+print "Hello World.\n";
+=pod
+comentarii
+documentatie
+etc.
+=cut
+print 'Hello World\n', "\n";
